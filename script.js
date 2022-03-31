9ea0663f48c00681cbaca7158d7e0d394a618f49
Kliueva-Kath <98223602+Kliueva-Kath@users.noreply.github.com>
2022-03-31 22:58:01 +0300

    feat: прописать скрипты

---
+++ script.js
@@ -0,0 +1,49 @@
+// сам попап
+let popup = document.querySelector(".popup");
+
+// кнопка открытия попапа (кнопка редактирования)
+let openPopupButton = document.querySelector(".profile__edit-button");
+
+// кнопка закрытия попапа
+let closePopupButton = document.querySelector(".popup__close-icon");
+
+// открытие попапа
+
+function openPopup() {
+  popup.classList.add("popup_opened");
+}
+openPopupButton.addEventListener("click", openPopup);
+
+// закрытие попапа
+
+function closePopup() {
+  popup.classList.remove("popup_opened");
+}
+closePopupButton.addEventListener("click", closePopup);
+
+// элемены имени и информации о себе в профиле
+let profileName = document.querySelector(".profile__name");
+let profileJob = document.querySelector(".profile__job");
+
+// Находим форму в DOM
+let formElement = document.querySelector(".edit-form");
+// Находим поля формы в DOM
+let nameInput = formElement.querySelector(".edit-form__name");
+nameInput.defaultValue = profileName.textContent;
+let jobInput = formElement.querySelector(".edit-form__job");
+jobInput.defaultValue = profileJob.textContent;
+
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function formSubmitHandler(evt) {
+  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+  // Так мы можем определить свою логику отправки.
+  // О том, как это делать, расскажем позже.
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+  popup.classList.remove("popup_opened");
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener("submit", formSubmitHandler);
