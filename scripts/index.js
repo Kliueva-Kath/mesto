29837347f1365461c3cb34e38671e6dd03d7bd9d
Ekaterina Kliueva <kliueva95@gmail.com>
2022-04-16 22:12:45 +0300

    fix: исправить js

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,30 @@
 // DOM-ЭЛЕМЕНТЫ
 
 // КАРТОЧКИ
-const initialCards = [
-  {
-    name: "Байкал",
-    link: "https://images.unsplash.com/photo-1551845041-63e8e76836ea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1889&q=80",
-  },
-  {
-    name: "Карелия",
-    link: "https://images.unsplash.com/photo-1573156667788-3b0a869a97b8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80",
-  },
-  {
-    name: "Красноярск",
-    link: "https://images.unsplash.com/photo-1597125760773-b0166e249ea7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1935&q=80",
-  },
-  {
-    name: "Новосиль",
-    link: "https://images.unsplash.com/photo-1444894423756-1bb106dce5a7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=771&q=80",
-  },
-  {
-    name: "Тулиновка",
-    link: "https://images.unsplash.com/photo-1516128935666-9742cf27e24c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80",
-  },
-  {
-    name: "Владивосток",
-    link: "https://images.unsplash.com/photo-1563941433-b6a094653ed2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=433&q=80",
-  },
+const initialCards = [{
+        name: "Байкал",
+        link: "https://images.unsplash.com/photo-1551845041-63e8e76836ea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1889&q=80",
+    },
+    {
+        name: "Карелия",
+        link: "https://images.unsplash.com/photo-1573156667788-3b0a869a97b8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80",
+    },
+    {
+        name: "Красноярск",
+        link: "https://images.unsplash.com/photo-1597125760773-b0166e249ea7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1935&q=80",
+    },
+    {
+        name: "Новосиль",
+        link: "https://images.unsplash.com/photo-1444894423756-1bb106dce5a7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=771&q=80",
+    },
+    {
+        name: "Тулиновка",
+        link: "https://images.unsplash.com/photo-1516128935666-9742cf27e24c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80",
+    },
+    {
+        name: "Владивосток",
+        link: "https://images.unsplash.com/photo-1563941433-b6a094653ed2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=433&q=80",
+    },
 ];
 
 const cardsContainer = document.querySelector(".elements__container");
@@ -34,15 +33,22 @@ const cardsTemplate = document.querySelector(".cards-template");
 // ПОПАП РЕДАКТИРОВАНИЯ
 const editPopup = document.querySelector(".popup_type_edit");
 
+// ФОРМА РЕДАКТИРОВАНИЯ
 const profileEditForm = document.forms.profileEditForm;
 const nameInput = profileEditForm.querySelector(".input__text_type_name");
 const jobInput = profileEditForm.querySelector(".input__text_type_job");
 
+// ФОРМА ДОБАВЛЕНИЯ
+const addCardForm = document.forms.addCardForm;
+const placeInputValue = addCardForm.querySelector(
+    ".input__text_type_place"
+).value;
+const linkInputValue = addCardForm.querySelector(
+    ".input__text_type_link"
+).value;
+
 const profileName = document.querySelector(".profile__name");
 const profileJob = document.querySelector(".profile__job");
-// значения по умолчанию для формы редактирования
-nameInput.defaultValue = profileName.textContent;
-jobInput.defaultValue = profileJob.textContent;
 
 // ПОПАП  ДОБАВЛЕНИЯ КАРТОЧЕК
 const addPopup = document.querySelector(".popup_type_add-card");
@@ -63,70 +69,64 @@ const openEditPopupButton = document.querySelector(".profile__edit-button");
 // ФУНКЦИИ
 
 function render() {
-  const html = initialCards.map(getElement);
-  cardsContainer.append(...html);
+    const html = initialCards.map(getElement);
+    cardsContainer.append(...html);
 }
 
 function getElement(item) {
-  const getElementTemplate = cardsTemplate.content.cloneNode(true);
-  const cardTitle = getElementTemplate.querySelector(".element__title");
-  const cardLink = getElementTemplate.querySelector(".element__photo");
-  const deleteButton = getElementTemplate.querySelector(
-    ".element__delete-button"
-  );
-  const likeButton = getElementTemplate.querySelector(".element__like");
+    const getElementTemplate = cardsTemplate.content.cloneNode(true);
+    const cardTitle = getElementTemplate.querySelector(".element__title");
+    const cardLink = getElementTemplate.querySelector(".element__photo");
+    const deleteButton = getElementTemplate.querySelector(
+        ".element__delete-button"
+    );
+    const likeButton = getElementTemplate.querySelector(".element__like");
 
-  cardTitle.textContent = item.name;
-  cardLink.src = item.link;
-  cardLink.alt = item.name;
-  deleteButton.addEventListener("click", deleteCard);
-  likeButton.addEventListener("click", function (evt) {
-    const eventTarget = evt.target;
-    eventTarget.classList.toggle("element__like_active");
-  });
-  cardLink.addEventListener("click", () => openImagePopup(item));
-  return getElementTemplate;
+    cardTitle.textContent = item.name;
+    cardLink.src = item.link;
+    cardLink.alt = item.name;
+    deleteButton.addEventListener("click", deleteCard);
+    likeButton.addEventListener("click", function(evt) {
+        const eventTarget = evt.target;
+        eventTarget.classList.toggle("element__like_active");
+    });
+    cardLink.addEventListener("click", () => openImagePopup(item));
+    return getElementTemplate;
 }
 
 function openImagePopup(element) {
-  openedImage.src = element.link;
-  openedImageTitle.textContent = element.name;
-  openedImage.alt = element.name;
-  imagePopup.classList.add("popup_opened");
+    openedImage.src = element.link;
+    openedImageTitle.textContent = element.name;
+    openedImage.alt = element.name;
+    openPopup(imagePopup);
 }
 
 function deleteCard(evt) {
-  const card = evt.target.closest(".element");
-  card.remove();
+    const card = evt.target.closest(".element");
+    card.remove();
 }
 
 function addFormHandler(evt) {
-  evt.preventDefault();
-  const addCardForm = document.forms.addCardForm;
-  const placeInputValue = addCardForm.querySelector(
-    ".input__text_type_place"
-  ).value;
-  const linkInputValue = addCardForm.querySelector(
-    ".input__text_type_link"
-  ).value;
-  const card = getElement({ name: placeInputValue, link: linkInputValue });
-  cardsContainer.prepend(card);
-  addPopup.classList.remove("popup_opened");
+    evt.preventDefault();
+    const card = getElement({ name: placeInputValue, link: linkInputValue });
+    cardsContainer.prepend(card);
+    addPopup.classList.remove("popup_opened");
+    addCardForm.reset();
 }
 
 function openPopup(popup) {
-  popup.classList.add("popup_opened");
+    popup.classList.add("popup_opened");
 }
 
 function editFormHandler(evt) {
-  evt.preventDefault();
-  profileName.textContent = nameInput.value;
-  profileJob.textContent = jobInput.value;
-  editPopup.classList.remove("popup_opened");
+    evt.preventDefault();
+    profileName.textContent = nameInput.value;
+    profileJob.textContent = jobInput.value;
+    editPopup.classList.remove("popup_opened");
 }
 
 function closePopup(popup) {
-  popup.classList.remove("popup_opened");
+    popup.classList.remove("popup_opened");
 }
 
 render();
@@ -135,7 +135,12 @@ render();
 
 addCardForm.addEventListener("submit", addFormHandler);
 
-openEditPopupButton.addEventListener("click", () => openPopup(editPopup));
+openEditPopupButton.addEventListener("click", () => {
+    openPopup(editPopup);
+    nameInput.defaultValue = profileName.textContent;
+    jobInput.defaultValue = profileJob.textContent;
+});
+
 openAddCardPopupButton.addEventListener("click", () => openPopup(addPopup));
 
 profileEditForm.addEventListener("submit", editFormHandler);
