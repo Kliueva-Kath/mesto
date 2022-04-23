9b8d9bf523341ff67f55d6c37a437352415e6910
Ekaterina Kliueva <kliueva95@gmail.com>
2022-04-23 23:49:45 +0300

    feat: добавить атрибуты инпутам

---
+++ scripts/validate.js
@@ -0,0 +1,8 @@
+enableValidation({
+    formSelector: ".popup__form",
+    inputSelector: ".popup__input",
+    submitButtonSelector: ".popup__button",
+    inactiveButtonClass: "popup__button_disabled",
+    inputErrorClass: "popup__input_type_error",
+    errorClass: "popup__error_visible",
+});
