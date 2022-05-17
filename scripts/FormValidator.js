9ab57657eecd270e26c730365774eb8a4ffc11f3
Kliueva-Kath <98223602+Kliueva-Kath@users.noreply.github.com>
2022-05-17 14:46:22 +0000

    feat: рефакторинг кода варидации по ООП

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -1,5 +1,5 @@
 const config = {
-    formSelector: ".form",
+   /*  formSelector: ".form", */
     inputSelector: ".form__input",
     buttonSelector: ".form__save-button",
     inactiveButtonClass: "form__save-button_inactive",
@@ -7,17 +7,91 @@ const config = {
     errorClass: "form__input-error_visible",
 };
 
-function enableValidation(config) {
+class FormValidator {
+	constructor(config, formSelector) {
+		this._form = formSelector;
+		this._input = config.inputSelector;
+		this._button = config.buttonSelector;
+		this._inactiveButton = config.inactiveButtonClass;
+		this._inputError = config.inputErrorClass;
+		this._error = config.errorClass;
+	}
+	
+	enableValidation() {
+		this._form.addEventListener("submit", (evt) => {
+			evt.preventDefault();
+		})
+		this._setEventListeners();
+	}
+	
+	_setEventListeners() {
+		this._toggleButtonState();
+
+		this._input.addEventListener("input", () => {
+            this._checkInputValidity();
+            this._toggleButtonState();
+        });
+	}
+
+	_checkInputValidity() {
+		 if (!this._input.validity.valid) {
+       this._showInputError();
+    } else {
+         this._hideInputError();
+    }
+	}
+
+	_showInputError() {
+		const errorElement = this._form.querySelector(`.${this._input.id}-error`);
+    this._input.classList.add(this._inputError);
+    errorElement.textContent = this._input.validationMessage;
+    errorElement.classList.add(this._error);
+	}
+
+	_hideInputError() {
+		const errorElement = this._form.querySelector(`.${this._input.id}-error`);
+     this._input.classList.remove(this._inputError);
+    errorElement.textContent = "";
+    errorElement.classList.remove(this._error);
+	}
+
+	_toggleButtonState() {
+    if (!this._form.checkValidity()) {
+        this._button.classList.add(this._inactiveButton);
+        this._button.setAttribute("disabled", true);
+    } else {
+        this._button.classList.remove(this._inactiveButton);
+        this._button.removeAttribute("disabled", true);
+    }
+	}
+
+	_disableButtonOnOpening() {
+    this._button.classList.add(this._inactiveButton);
+    this._button.setAttribute("disabled", true);
+	}
+	
+}
+
+const profileEditFormValidator = new FormValidator(config, document.forms.profileEditForm);
+
+editFormValidator.enableValidation();
+
+const cardAddingFormValidator = new FormValidator(config, document.forms.addCardForm);
+
+cardAddingFormValidator.enableValidation();
+
+
+/* function enableValidation(config) {
     const forms = Array.from(document.querySelectorAll(config.formSelector));
     forms.forEach((form) => {
         form.addEventListener("submit", (evt) => {
             evt.preventDefault();
         });
         setEventListeners(form, config);
-    });
+    }); */
 }
 
-function setEventListeners(form, config) {
+/* function setEventListeners(form, config) {
     toggleButtonState(form, config);
     const inputs = Array.from(form.querySelectorAll(config.inputSelector));
     inputs.forEach((input) => {
@@ -26,17 +100,17 @@ function setEventListeners(form, config) {
             toggleButtonState(form, config);
         });
     });
-}
+} */
 
-function checkInputValidity(form, input, config) {
+/* function checkInputValidity(form, input, config) {
     if (!input.validity.valid) {
         showInputError(form, input, input.validationMessage, config);
     } else {
         hideInputError(form, input, config);
     }
-}
+} */
 
-function showInputError(form, input, errorMessage, config) {
+/* function showInputError(form, input, errorMessage, config) {
     const errorElement = form.querySelector(`.${input.id}-error`);
     input.classList.add(config.inputErrorClass);
     errorElement.textContent = errorMessage;
@@ -48,9 +122,9 @@ function hideInputError(form, input, config) {
     input.classList.remove(config.inputErrorClass);
     errorElement.textContent = "";
     errorElement.classList.remove(config.errorClass);
-}
+} */
 
-function toggleButtonState(form, config) {
+/* function toggleButtonState(form, config) {
     const button = form.querySelector(config.buttonSelector);
     if (!form.checkValidity()) {
         button.classList.add(config.inactiveButtonClass);
@@ -59,12 +133,12 @@ function toggleButtonState(form, config) {
         button.classList.remove(config.inactiveButtonClass);
         button.removeAttribute("disabled", true);
     }
-}
+} */
 
-function disableButtonOnOpening(form, config) {
+/* function disableButtonOnOpening(form, config) {
     const button = form.querySelector(config.buttonSelector);
     button.classList.add(config.inactiveButtonClass);
     button.setAttribute("disabled", true);
-}
+} */
 
-enableValidation(config);
+/* enableValidation(config); */
