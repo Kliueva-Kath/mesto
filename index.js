01f7b1015885ab7b6c0c10eef7e2b9f3af516176
Kliueva-Kath <98223602+Kliueva-Kath@users.noreply.github.com>
2022-05-19 08:05:51 +0000

    переместить создание карточек в index

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { FormValidator } from "./FormValidator.js"
+import { Card } from "./Card.js"
 
 // DOM-ЭЛЕМЕНТЫ
 
@@ -163,6 +164,9 @@ const config = {
     errorClass: "form__input-error_visible" 
 };
 
+
+
+
 const profileEditFormValidator = new FormValidator(config, document.forms.profileEditForm);
 
 profileEditFormValidator.enableValidation();
@@ -170,6 +174,41 @@ profileEditFormValidator.enableValidation();
 const cardAddingFormValidator = new FormValidator(config, document.forms.addCardForm);
 
 cardAddingFormValidator.enableValidation();
+
+
+// добавление карточек на страницу
+
+const initialCards = [{
+        name: "Байкал",
+        link: "https://images.unsplash.com/photo-1551845041-63e8e76836ea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1889&q=80",
+    },
+    {
+        name: "Карелия",
+        link: "https://images.unsplash.com/photo-1573156667788-3b0a869a97b8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80",
+    },
+    {
+        name: "Красноярск",
+        link: "https://images.unsplash.com/photo-1597125760773-b0166e249ea7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1935&q=80",
+    },
+    {
+        name: "Новосиль",
+        link: "https://images.unsplash.com/photo-1444894423756-1bb106dce5a7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=771&q=80",
+    },
+    {
+        name: "Тулиновка",
+        link: "https://images.unsplash.com/photo-1516128935666-9742cf27e24c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80",
+    },
+    {
+        name: "Владивосток",
+        link: "https://images.unsplash.com/photo-1563941433-b6a094653ed2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=433&q=80",
+    },
+];
+
+initialCards.forEach((item) => {
+    const card = new Card(item, ".cards-template");
+    const cardElement = card.generateCard();
+    document.querySelector(".elements__container").append(cardElement);
+});
 
 
 export { openPopup, closePopup };
