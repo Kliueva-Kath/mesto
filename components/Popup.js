59d7b49f4edc25887028c684002a684d223dbfbf
Kliueva-Kath <98223602+Kliueva-Kath@users.noreply.github.com>
2022-05-29 03:56:17 +0300

    feat: начать делать класс Popup

---
+++ components/Popup.js
@@ -0,0 +1,37 @@
+class Popup {
+    constructor(popupSelector) {
+        this._popup = popupSelector;
+    }
+
+    open() {
+        this._popup.classList.add("popup_opened");
+    }
+
+    close() {
+        this._popup.classList.remove("popup_opened");
+    }
+
+    _handleEscClose(evt) {
+        if (evt.key === "Escape") {
+            this._popup.close();
+        }
+    }
+
+    setEventListeners() {
+        // закрытие по клику на кнопку закрытия
+        const popupCloseButton = this._popup.querySelector(".popup__close-icon");
+        popupCloseButton.addEventListener("click", () => {
+            this._popup.close();
+        });
+
+        // закрытие кликом по оверлею
+        const overlays = Array.from(document.querySelectorAll(".popup"));
+        overlays.forEach((overlay) => {
+            overlay.addEventListener("mousedown", (evt) => {
+                if (evt.target === evt.currentTarget) {
+                    this._popup.close();
+                }
+            });
+        });
+    }
+}
