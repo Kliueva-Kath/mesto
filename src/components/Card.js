a621540091aebe0750869da64a142109afd83cde
Kliueva-Kath <98223602+Kliueva-Kath@users.noreply.github.com>
2022-06-06 02:01:47 +0300

    fix: вынести элементы фото и лайка в классе Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,70 +1,66 @@
 export default class Card {
-  constructor(data, handleCardClick, cardSelector) {
-    this._cardSelector = cardSelector;
-    this._name = data.name;
-    this._link = data.link;
-    this._handleCardClick = handleCardClick;
-  }
+    constructor(data, handleCardClick, cardSelector) {
+        this._cardSelector = cardSelector;
+        this._name = data.name;
+        this._link = data.link;
+        this._handleCardClick = handleCardClick;
+    }
 
-  // получение разметки template-элемента и вставка данных в разметку
+    // получение разметки template-элемента и вставка данных в разметку
 
-  _getTemplate() {
-    const cardElement = document
-      .querySelector(this._cardSelector)
-      .content.querySelector(".element")
-      .cloneNode(true);
+    _getTemplate() {
+        const cardElement = document
+            .querySelector(this._cardSelector)
+            .content.querySelector(".element")
+            .cloneNode(true);
 
-    return cardElement;
-  }
+        return cardElement;
+    }
 
-  // создание карточки
+    // создание карточки
 
-  generateCard() {
-    this._element = this._getTemplate();
+    generateCard() {
+        this._element = this._getTemplate();
 
-    this._element.querySelector(".element__photo").src = this._link;
-    this._element.querySelector(".element__title").textContent = this._name;
-    this._element.querySelector(".element__photo").alt = this._name;
+        this._photo = this._element.querySelector(".element__photo");
+        this._photo.src = this._link;
+        this._element.querySelector(".element__title").textContent = this._name;
+        this._photo.alt = this._name;
 
-    this._setEventListeners();
+        this._setEventListeners();
 
-    return this._element;
-  }
+        return this._element;
+    }
 
-  // удаление карточки
+    // удаление карточки
 
-  _handleDeleteCard() {
-    this._element.remove();
-  }
+    _handleDeleteCard() {
+        this._element.remove();
+    }
 
-  _handleLikePost() {
-    this._element
-      .querySelector(".element__like")
-      .classList.toggle("element__like_active");
-  }
+    _handleLikePost() {
+        this._likeButton.classList.toggle("element__like_active");
+    }
 
-  // слушатели событий
+    // слушатели событий
 
-  _setEventListeners() {
-    // событие удаления карточки
-    this._element
-      .querySelector(".element__delete-button")
-      .addEventListener("click", () => {
-        this._handleDeleteCard();
-      });
+    _setEventListeners() {
+        // событие удаления карточки
+        this._element
+            .querySelector(".element__delete-button")
+            .addEventListener("click", () => {
+                this._handleDeleteCard();
+            });
 
-    // событие лайка
-    this._element
-      .querySelector(".element__like")
-      .addEventListener("click", () => {
-        this._handleLikePost();
-      });
+        // событие лайка
+        this._likeButton = this._element.querySelector(".element__like");
+        this._likeButton.addEventListener("click", () => {
+            this._handleLikePost();
+        });
 
-    // событие открытия попапа
-    this._element
-      .querySelector(".element__photo")
-      .addEventListener("click", () => {
-        this._handleCardClick(this._name, this._link);
-      });
-  }
+        // событие открытия попапа
+        this._photo.addEventListener("click", () => {
+            this._handleCardClick(this._name, this._link);
+        });
+    }
 }
