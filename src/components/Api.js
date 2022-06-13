fd77248c04bebc3fbcac1c860abedbc905a2c3b6
Kliueva-Kath <98223602+Kliueva-Kath@users.noreply.github.com>
2022-06-13 22:27:59 +0300

    feat: добавить разметку счетчика лайков

---
+++ src/components/Api.js
@@ -0,0 +1,62 @@
+export default class Api {
+    constructor({ baseUrl, headers }) {
+        this._url = baseUrl;
+        this._headers = headers;
+    }
+
+    getUserInfo() {
+        return (fetch(`${this._url}/users/me`), {
+            method: GET,
+            headers: this._headers,
+        }).then((res) => {
+            if (res.ok) {
+                return res.json();
+            }
+            return Promise.reject(`Ошибка: ${res.status}`);
+        });
+    }
+
+    getInitialCards() {
+        return (fetch(`${this._url}/cards`), {
+            method: GET,
+            headers: this._headers,
+        }).then((res) => {
+            if (res.ok) {
+                return res.json();
+            }
+            return Promise.reject(`Ошибка: ${res.status}`);
+        });
+    }
+
+    editUserInfo(userName, userAbout) {
+        return (fetch(`${this._url}/users/me`), {
+            method: PATCH,
+            headers: this._headers,
+            body: JSON.stringify({
+                name: userName,
+                about: userAbout,
+            }),
+        }).then((res) => {
+            if (res.ok) {
+                return res.json();
+            }
+            return Promise.reject(`Ошибка: ${res.status}`);
+        });
+    }
+
+    addNewCard(cardName, cardLink) {
+        return (fetch(`${this._url}/cards`), {
+            method: POST,
+            headers: this._headers,
+            body: JSON.stringify({
+                name: cardName,
+                link: cardLink,
+            }),
+        }).then((res) => {
+            if (res.ok) {
+                return res.json();
+            }
+            return Promise.reject(`Ошибка: ${res.status}`);
+        });
+    }
+}
---
+++ src/components/PopupWithConfirmation.js
@@ -0,0 +1,3 @@
+import Popup from "./Popup.js";
+
+export default class PopupWithConfirmation extends Popup {}
